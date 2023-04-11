2562a38c0df2012f5097ee0656e2029b6d44f91b
Yofandra <drayofan@gmail.com>
2023-04-11 15:51:27 +0700

    first commit

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $table="mahasiswa";
+    public $timestamps= false;
+    protected $primaryKey = 'Nim';
+
+    protected $fillable=[
+        'Nim',
+        'Nama',
+        'Email',
+        'Tanggal_Lahir',
+        'kelas_id',
+        'Jurusan',
+        'No_Handphone'
+    ];
+
+    public function kelas(){
+        return $this->belongsTo(Kelas::class);
+    }
+}
